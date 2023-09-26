a05c9ba1049915e26c75edc891e5e59fd6e607c5
Bett Enock <bettenock968@gmail.com>
2023-09-26 09:14:59 -0700

    add sql snippets

--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+-- Table: VET1.animals
+
+-- DROP TABLE IF EXISTS "VET1".animals;
+
+CREATE TABLE IF NOT EXISTS "VET1".animals
+(
+    id integer NOT NULL,
+    name text COLLATE pg_catalog."default" NOT NULL,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric,
+    CONSTRAINT animals_pkey PRIMARY KEY (id)
+)
+
+
+ALTER TABLE IF EXISTS "VET1".animals
+    OWNER to postgres;
